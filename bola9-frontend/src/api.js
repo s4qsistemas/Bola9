76de9c7b761d59b470c1f48225c6e41c2d57682b
Diego Alvial <sigm4q@gmail.com>
2026-08-12 14:52:56 -0400

    feat: implementación de docker en vps

--- bola9-frontend/src/api.js
+++ bola9-frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Si existe la variable (Producción), la usa. 
-// Si NO existe (Desarrollo/Ngrok), usa el string vacío '' para que el Proxy de Vite actúe.
-const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+// Si NO existe (Desarrollo/Ngrok), usa el BASE_URL (ej. /bola9/api) para que el Proxy actúe.
+const API_BASE_URL = import.meta.env.VITE_API_URL || import.meta.env.BASE_URL + 'api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
